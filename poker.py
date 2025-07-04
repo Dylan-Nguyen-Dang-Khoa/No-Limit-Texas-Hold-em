821692e70ebbc3bbb018dbc4ce95871b80bcddb0
Dylan_Nguyen_Dang_Khoa <Dylan_Nguyen_Dang_Khoa@s2023.ssts.edu.sg>
2025-07-04 12:15:55 +0800

    Fixed the bug where the max resets after checking every player, which could cause multiple winners even if there aren't. Moved it outside to be inside for i in range(1, len(tie_breaker_comparisons[0])+1), so it resets after checking one singular position for all tiebreaker participants

--- poker.py
+++ poker.py
@@ -750,9 +750,9 @@ class HandEvaluator:
                 tie_breaker_comparisons.append(
                     self.n_of_a_kind_tiebreaker(card_ranks, player_index, frequency)
                 )
-        for i in range(1, len(tie_breaker_comparisons[0])):
+        for i in range(1, len(tie_breaker_comparisons[0])+1):
+            max = 0
             for player in tie_breaker_comparisons:
-                max = 0
                 if player[i] > max:
                     max = player[i]
                     winner = [player[0]]
